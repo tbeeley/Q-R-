e9cfff58eb4d4be0f34c9d36ec3c509aebaedcd9
Thomas Beeley <tbeeley@gmail.com>
2014-12-01 11:23:28 +0000

    Quarter knows when it ended

--- lib/quarter.rb
+++ lib/quarter.rb
@@ -1,10 +1,11 @@
 class Quarter
 
-	def initialize(common_stock_positions)
+	def initialize(common_stock_positions, ending)
 		@common_stock_positions = common_stock_positions
+		@ending = ending
 	end
 
-	attr_accessor :common_stock_positions
+	attr_accessor :common_stock_positions, :ending
 
 	def total_value
 		common_stock_positions.values.inject {|a, b| a + b}
--- spec/quarter_spec.rb
+++ spec/quarter_spec.rb
@@ -2,13 +2,17 @@ require 'quarter'
 
 describe Quarter do
 
-	let(:quarter) { Quarter.new(data) }
+	let(:quarter) { Quarter.new(data, "3/31/12") }
 	let(:data) 	  { {:AGILENT_TECHNOLOGIES_INC => 455, :ALCOA_INC => 322, :AARONS_INC => 636} }
 
 	context 'when initialized' do
 
 		it 'should have common stock positions' do
 			expect(quarter.common_stock_positions).to be_an_instance_of Hash
+		end
+
+		it 'should have a time period' do
+			expect(quarter.ending).to eq "3/31/12"
 		end
 
 	end
