af3569738476e73437c9a67e32fb1c43239e3352
Thomas Beeley <tbeeley@gmail.com>
2014-12-01 11:05:12 +0000

    Quarter knows total value

--- lib/quarter.rb
+++ lib/quarter.rb
@@ -6,4 +6,8 @@ class Quarter
 
 	attr_accessor :common_stock_positions
 
+	def total_value
+		common_stock_positions.values.inject {|a, b| a + b}
+	end
+
 end
--- spec/quarter_spec.rb
+++ spec/quarter_spec.rb
@@ -3,12 +3,20 @@ require 'quarter'
 describe Quarter do
 
 	let(:quarter) { Quarter.new(data) }
-	let(:data) 	 { [{"AGILENT TECHNOLOGIES INC" => 455, "ALCOA INC" => 322, "AARONS INC" => 322}] }
+	let(:data) 	  { {:AGILENT_TECHNOLOGIES_INC => 455, :ALCOA_INC => 322, :AARONS_INC => 636} }
 
 	context 'when initialized' do
 
 		it 'should have common stock positions' do
-			expect(quarter.common_stock_positions).to eq [{"AGILENT TECHNOLOGIES INC" => 455, "ALCOA INC" => 322, "AARONS INC" => 322}]
+			expect(quarter.common_stock_positions).to be_an_instance_of Hash
+		end
+
+	end
+
+	context 'Quarter should know' do
+
+		it 'the total value of CSM_positions' do
+			expect(quarter.total_value).to eq 1413
 		end
 
 	end
