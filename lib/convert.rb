5f3d629336baba7d6af0550da59952533db37d33
Thomas Beeley <tbeeley@gmail.com>
2014-12-03 17:20:36 +0000

    Convert class now initializes an object with the contents of the txt file

--- lib/convert.rb
+++ lib/convert.rb
@@ -1,3 +1,21 @@
 class Convert
 
+	def initialize(text_file)
+		@contents = contents
+		read_file(text_file)
+	end
+
+	attr_accessor :contents
+
+	def read_file(text_file)
+		file = File.open(text_file, "r")
+		@contents = file.read
+	end
+
+	def file_open(text_file)
+		File.open("sample.txt").readlines.each do |line|
+			puts line
+		end
+	end
+
 end
--- spec/convert_spec.rb
+++ spec/convert_spec.rb
@@ -2,4 +2,18 @@ require 'Convert'
 
 describe Convert do
 
+	let(:march_data) { Convert.new('march_small.txt')}
+
+	context 'when initialized' do
+
+		it 'should take in a text file' do
+			(march_data.contents).should include('APPLE INC')
+		end
+
+		it 'should convert it to a string' do
+			expect(march_data.contents).to be_an_instance_of String
+		end
+
+	end
+
 end
